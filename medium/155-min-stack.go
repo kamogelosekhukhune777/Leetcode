e587cee29eff9756abbe28c12d436a7410380d51
Kamogelo Sekhukhune <kamogelosekhukhune7@gmail.com>
2023-10-28 18:38:47 +0200

    155-Min-Stack

--- medium/155-min-stack.go
+++ medium/155-min-stack.go
@@ -5,12 +5,12 @@ Design a stack that supports push, pop, top, and retrieving the minimum element 
 
 Implement the MinStack class:
 
-MinStack() initializes the stack object.
-void push(int val) pushes the element val onto the stack.
-void pop() removes the element on the top of the stack.
-int top() gets the top element of the stack.
-int getMin() retrieves the minimum element in the stack.
-You must implement a solution with O(1) time complexity for each function.
+-MinStack() initializes the stack object.
+-void push(int val) pushes the element val onto the stack.
+-void pop() removes the element on the top of the stack.
+-int top() gets the top element of the stack.
+-int getMin() retrieves the minimum element in the stack.
+-You must implement a solution with O(1) time complexity for each function.
 
 Example 1:
 
@@ -37,3 +37,35 @@ Constraints:
 Methods pop, top and getMin operations will always be called on non-empty stacks.
 At most 3 * 104 calls will be made to push, pop, top, and getMin.
 */
+
+type MinStack struct {
+	stack    []int
+	minStack []int
+}
+
+func Constructor() MinStack {
+	return MinStack{}
+}
+
+func (this *MinStack) Push(val int) {
+	this.stack = append(this.stack, val)
+	if len(this.minStack) == 0 || val <= this.minStack[len(this.minStack)-1] {
+		this.minStack = append(this.minStack, val)
+	}
+}
+
+func (this *MinStack) Pop() {
+	if this.stack[len(this.stack)-1] == this.minStack[len(this.minStack)-1] {
+		this.minStack = this.minStack[:len(this.minStack)-1]
+	}
+	this.stack = this.stack[:len(this.stack)-1]
+}
+
+func (this *MinStack) Top() int {
+	return this.stack[len(this.stack)-1]
+
+}
+
+func (this *MinStack) GetMin() int {
+	return this.minStack[len(this.minStack)-1]
+}
