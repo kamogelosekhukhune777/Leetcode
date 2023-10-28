8a65f842bc1d9b021529320a34bd4f3d034b426d
Kamogelo Sekhukhune <kamogelosekhukhune7@gmail.com>
2023-10-28 23:36:45 +0200

    20-valid-parentheses

--- easy/20-valid-parentheses.go
+++ easy/20-valid-parentheses.go
@@ -1,14 +1,14 @@
 package easy
 
 /*
-Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+determine if the input string is valid.
 
 An input string is valid if:
-
-Open brackets must be closed by the same type of brackets.
-Open brackets must be closed in the correct order.
-Every close bracket has a corresponding open bracket of the same type.
- ------------------------------------------------
+1.Open brackets must be closed by the same type of brackets.
+2.Open brackets must be closed in the correct order.
+3.Every close bracket has a corresponding open bracket of the same type.
+-------------------------------------------------
 Example 1:
 
 Input: s = "()"
@@ -27,3 +27,23 @@ Constraints:
 1 <= s.length <= 104
 s consists of parentheses only '()[]{}'.
 */
+
+func IsValid(s string) bool {
+	array := []rune{}
+	bucketMap := map[rune]rune{
+		'}': '{',
+		']': '[',
+		')': '(',
+	}
+	for _, char := range s {
+		if char == '{' || char == '(' || char == '[' {
+			array = append(array, char)
+		} else {
+			if len(array) == 0 || array[len(array)-1] != bucketMap[char] {
+				return false
+			}
+			array = array[:len(array)-1]
+		}
+	}
+	return len(array) == 0
+}
