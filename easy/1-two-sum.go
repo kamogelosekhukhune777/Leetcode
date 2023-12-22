9c6921eacef1af38d331aa0af51aa06150be2871
Kamogelo Sekhukhune <kamogelosekhukhune7@gmail.com>
2023-12-22 12:13:30 +0200

    1-two-sum

--- easy/1-two-sum.go
+++ easy/1-two-sum.go
@@ -9,26 +9,38 @@ element twice.
 
 You can return the answer in any order.
 
-
 Example 1:
-
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+   Input: nums = [2,7,11,15], target = 9
+   Output: [0,1]
+   Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 Example 2:
+   Input: nums = [3,2,4], target = 6
+   Output: [1,2]
 
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
 Example 3:
-
-Input: nums = [3,3], target = 6
-Output: [0,1]
-
+   Input: nums = [3,3], target = 6
+   Output: [0,1]
 
 Constraints:
--  2 <= nums.length <= 104
--  -109 <= nums[i] <= 109
--  -109 <= target <= 109
+    2 <= nums.length <= 104
+   -109 <= nums[i] <= 109
+   -109 <= target <= 109
+
 Only one valid answer exists.
 */
+
+func TwoSum(nums []int, target int) []int {
+	numIndicies := make(map[int]int)
+
+	for i, num := range nums {
+		complement := target - num
+
+		if index, ok := numIndicies[complement]; ok {
+			return []int{index, i}
+		}
+
+		numIndicies[num] = i
+	}
+	return nil
+}
